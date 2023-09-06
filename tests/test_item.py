f92cc6c4a116c5ae7db4aefeb44231713154bc38
Kryazhov Roman <kryazhov.roman@mail.ru>
2023-09-07 04:06:51 +0600

    fixture

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,23 +1,25 @@
+import pytest
 from src.item import Item
 
 
-def test_all_items():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
+@pytest.fixture
+def item1():
+    item = Item("Смартфон", 10000, 20)
+    return item
 
+@pytest.fixture
+def item2():
+    item = Item("Ноутбук", 20000, 5)
+    return item
+
+def test_all_items(item1, item2):
     assert Item.all == [item1, item2]
 
-def test_calculate_total_price():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-
+def test_calculate_total_price(item1, item2):
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
 
-def test_apply_discount():
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-
+def test_apply_discount(item1, item2):
     Item.pay_rate = 0.8
     item1.apply_discount()
 
